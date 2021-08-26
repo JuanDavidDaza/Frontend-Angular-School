c1fc8515c4460fcd79fd475bfe2c5a743c0448d7
Juan David <juanda7473@gmail.com>
2021-08-26 12:58:02 -0500

    update frontend

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { RegisterTeacherComponent } from "./teacher/register-teacher/register-teacher.component";
+import { RegisterStudentComponent } from "./student/register-student/register-student.component";
+import { RegisterCoursesComponent } from "./courses/register-courses/register-courses.component";
+import { RegisterMatterComponent } from "./courses/register-matter/register-matter.component";
+import { LoginComponent } from "./login/login.component";
+
+const routes: Routes = [
+  {
+    path:'',
+    component:LoginComponent,
+    pathMatch: 'full',
+  },
+  {
+    path:'registerStudent',
+    component:RegisterStudentComponent,
+  },
+  {
+    path:'login',
+    component:LoginComponent,
+  },
+  {
+    path:'registerTeacher',
+    component:RegisterTeacherComponent,
+  },
+  {
+    path:'registerCourses',
+    component:RegisterCoursesComponent,
+  },
+  {
+    path:'registerMatter',
+    component:RegisterMatterComponent,
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,67 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './home/header/header.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { RegisterTeacherComponent } from './teacher/register-teacher/register-teacher.component';
+import { RegisterStudentComponent } from './student/register-student/register-student.component';
+import { RegisterCoursesComponent } from './courses/register-courses/register-courses.component';
+import { RegisterMatterComponent } from './courses/register-matter/register-matter.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//servicios
+import { CoursesService } from './services/courses.service';
+import { StudentService } from './services/student.service';
+import { TeacherService } from './services/teacher.service';
+
+//Form dinamicos de Angular
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+//Componentes de Angular Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { LoginComponent } from './login/login.component';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterTeacherComponent,
+    RegisterStudentComponent,
+    RegisterCoursesComponent,
+    RegisterMatterComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatInputModule,
+    MatSnackBarModule,
+    MatDatepickerModule,
+  ],
+  providers: [
+    //servicios
+    CoursesService,
+    StudentService,
+    TeacherService,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
